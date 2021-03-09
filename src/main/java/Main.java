3758af0a1cbb5f9bb3b06363833bcdfc84b1d5cb
Nataliia Gavaleshko <n.gavaleshko@gmail.com>
2021-03-09 21:32:49 +0200

    gradle and testing classes

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Secret secret = new Secret();
+        secret.setSuperPower("Odri");
+        System.out.println(secret.getSuperPower());
+    }
+}
